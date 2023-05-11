8be45cbcc8d1b9ed1fe0f91f7c6346e4cd193840
danmohedano <danimohedano1998@gmail.com>
2023-05-11 13:44:33 +0200

    Added treesitter config

---
+++ lua/dan/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "julia", "rust", "python", "markdown", "markdown_inline", "nix"},
+            sync_install = false,
+            ignore_install = { "" },
+            highlight = {
+                enable = true,
+                disable = { "" },
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true, disable = { "yaml" } },
+            rainbow = {
+                enable = true,
+                extended_mode = true,
+                max_file_lines = nil,
+            }
+        })
+    end,
+}
