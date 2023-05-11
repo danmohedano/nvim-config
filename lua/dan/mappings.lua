848234803526847173639d578a0f7da2abe4af82
danmohedano <danimohedano1998@gmail.com>
2023-05-11 10:48:04 +0200

    Nvimtree config

--- lua/dan/mappings.lua
+++ lua/dan/mappings.lua
@@ -133,18 +133,9 @@ if is_available "Comment.nvim" then
 end
 
 -- NEOTREE - FILE EXPLORER --
-if is_available "neo-tree.nvim" then
-    maps.n["<leader>e"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" }
-    maps.n["<leader>o"] = {
-        function()
-            if vim.bo.filetype == "neo-tree" then
-                vim.cmd.wincmd "p"
-            else
-                vim.cmd.Neotree "focus"
-            end
-        end,
-        desc = "Toggle Explorer Focus",
-    }
+if is_available "nvim-tree.lua" then
+    maps.n["<leader>e"] = { "<cmd>NvimTreeToggle<cr>", desc = "Toggle Explorer" }
+    maps.n["<leader>o"] = { "<cmd>NvimTreeFocus<cr>", desc = "Toggle Explorer Focus" }
 end
 
 -- MASON PACKAGE MANAGER --
