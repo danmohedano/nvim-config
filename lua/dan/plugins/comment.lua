ff7c6a7920a9271ee9793949bac2387a1cd72cea
danmohedano <danimohedano1998@gmail.com>
2023-06-05 20:56:31 +0200

    Comments - Updated config

--- lua/dan/plugins/comment.lua
+++ lua/dan/plugins/comment.lua
@@ -1,6 +1,18 @@
+local setup_config = {
+    padding = true,
+    sticky = true,
+    ignore = nil,
+    toggler = { line = 'gcc', block = 'gbc' },
+    opleader = { line = 'gc', block = 'gb' },
+    extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+    mappings = { basic = true, extra = true },
+    pre_hook = nil,
+    post_hook = nil,
+}
+
 return {
     "numToStr/Comment.nvim",
     config = function()
-        require("Comment").setup({})
+        require("Comment").setup(setup_config)
     end,
 }
