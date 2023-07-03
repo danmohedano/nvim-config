8f650588e6fe97b91e12a7e80a9ff61b8a9efb0c
danmohedano <danimohedano1998@gmail.com>
2023-07-03 13:11:48 +0200

    LSP - Updated keymaps

--- lua/dan/plugins/lsp.lua
+++ lua/dan/plugins/lsp.lua
@@ -34,16 +34,20 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set({"n", "x"}, "<F3>", function() vim.lsp.buf().format({async = true}) end, opts)
+    vim.keymap.set("n", "<F4>", function() vim.lsp.buf.code_action() end, opts)
+
+    vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<leader>vsh", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
