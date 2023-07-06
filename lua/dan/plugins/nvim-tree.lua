1fdda6cde3ffa9a266517ea8c9b5e08e84b2c5f7
danmohedano <danimohedano1998@gmail.com>
2023-07-06 14:13:15 +0200

    fix: activated autoclosing when opening file in nvim-tree

--- lua/dan/plugins/nvim-tree.lua
+++ lua/dan/plugins/nvim-tree.lua
@@ -254,7 +254,7 @@ nvim_tree.setup({ -- BEGIN_DEFAULT_OPTS
             },
         },
         open_file = {
-            quit_on_open = false,
+            quit_on_open = true,
             resize_window = true,
             window_picker = {
                 enable = true,
